f81adee911c5e0ce56a2147494c67cb7ee04ffb8
Md-Azad <sokalazad@gmail.com>
2023-03-21 22:00:36 +0100

    Fetched data from api using useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   // const [count, setCount] = useState(55);
@@ -8,11 +8,42 @@ function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <User></User>
       {nayoks.map((nayok) => (
         <Person name={nayok}></Person>
       ))}
-      {/* <Person name=" Azad Hossain" job="Student"></Person>
-      <Person name="sazzad Hossain" job="Driver"></Person> */}
+    </div>
+  );
+}
+
+// Fetch data from Api and show them
+
+function User() {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+  return (
+    <div>
+      <h1>name: {users.length}</h1>
+      {/* {users.map((user) => (
+        <li>{user.name}</li>
+      ))} */}
+      {users.map((user) => (
+        <ShowUser name={user.name} email={user.email}></ShowUser>
+      ))}
+    </div>
+  );
+}
+// component for showing api's data
+
+function ShowUser(props) {
+  return (
+    <div>
+      <h1>name: {props.name}</h1>
+      <p>Email: {props.email}</p>
     </div>
   );
 }
