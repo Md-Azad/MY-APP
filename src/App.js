ad61d2b6bee96cee4f21d2296bd65f3a33436f3c
Md-Azad <sokalazad@gmail.com>
2023-03-21 16:37:19 +0100

    besic reactJs concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // const [count, setCount] = useState(55);
+  const nayoks = ["rubel", "bapparaj", "jhankar", "mahbub", "shalmanshah"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      {nayoks.map((nayok) => (
+        <Person name={nayok}></Person>
+      ))}
+      {/* <Person name=" Azad Hossain" job="Student"></Person>
+      <Person name="sazzad Hossain" job="Driver"></Person> */}
     </div>
   );
 }
 
+// Making component
+
+function Person(props) {
+  return (
+    <div>
+      <h1>name:-{props.name} </h1>
+      <h2>Job:{props.job} </h2>
+    </div>
+  );
+}
+
+// Counter
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseValue = () => setCount(count + 1);
+  const decreaseValue = () => {
+    if (count === 0) {
+      alert("the value can't be decrease more!!!");
+    } else {
+      setCount(count - 1);
+    }
+  };
+  const reset = () => setCount(0);
+  return (
+    <div>
+      <h1>Counter:{count}</h1>
+      <button onClick={increaseValue}>Increase</button>
+      <button onClick={decreaseValue}>Decrease</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+}
 export default App;
